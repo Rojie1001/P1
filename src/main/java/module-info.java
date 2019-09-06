612a9a36624dfce2d6bd9ad055a1e56e4cb0c109
Rojie1001 <33533062+Rojie1001@users.noreply.github.com>
2019-09-06 19:33:29 -0300

    Method Cadastrar
    criado método para ir para a tela de cadastro

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module rojie.poo.ifsc.P1 {
     requires javafx.controls;
     requires javafx.fxml;
+	requires javafx.base;
 
     opens rojie.poo.ifsc.P1 to javafx.fxml;
     exports rojie.poo.ifsc.P1;
--- src/main/java/rojie/poo/ifsc/P1/LoginController.java
+++ src/main/java/rojie/poo/ifsc/P1/LoginController.java
@@ -1,34 +1,49 @@
 package rojie.poo.ifsc.P1;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginController {
 
-
 	@FXML
 	Label lblEntraSis;
-	
+
 	@FXML
 	Label lblUser;
-	
+
 	@FXML
 	Label lblPass;
-	
+
 	@FXML
 	TextField txtGetUser;
-	
+
 	@FXML
 	TextField txtGetPass;
-	
+
 	@FXML
 	Button btnEntrar;
-	
+
 	@FXML
 	Button btnCadastro;
+
 	
+
+	public void Cadastrar() throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("cadastro.fxml"));
+		Parent root = (Parent) fxmlLoader.load();
 	
+		Stage stage = new Stage();
+		stage.setScene(new Scene(root));
+		stage.show();
+	}
+
 }
