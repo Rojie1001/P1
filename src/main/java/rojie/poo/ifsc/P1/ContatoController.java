0eda188bd0feda594c77ebba15d16dfd7b3822e0
Rojie1001 <33533062+Rojie1001@users.noreply.github.com>
2019-09-07 19:11:43 -0300

    point safe

--- src/main/java/rojie/poo/ifsc/P1/ContatoController.java
+++ src/main/java/rojie/poo/ifsc/P1/ContatoController.java
@@ -2,25 +2,44 @@ package rojie.poo.ifsc.P1;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
 
 public class ContatoController implements Initializable {
 
 	@FXML
-	private ChoiceBox<String> choicebox1 =  new ChoiceBox<>();
+	private ChoiceBox<String> choicebox1 = new ChoiceBox<>();
+	@FXML
+	private Label lblInfo;
+
+	@FXML
+	private ChoiceBox<String> choicebox2 = new ChoiceBox<>();
+
+	@FXML
+	private CheckBox chkAgradecimento;
+
+	@FXML
+	private CheckBox chkElogio;
+
+	@FXML
+	private CheckBox chkReclamacao;
+
+	@FXML
+	private CheckBox chkSugestao;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		choicebox1.getItems().add("teste");
-		choicebox1.getItems().add("teste 2");
-		
-	}
+		choicebox1.getItems().add("Acadêmico");
+		choicebox1.getItems().add("Administrativo");
 
+		choicebox2.getItems().add("Infraestrutura");
+		choicebox2.getItems().add("Cursos");
+		choicebox2.getItems().add("Servidores");
+		choicebox2.getItems().add("Limpeza");
+
+	}
 
 }
